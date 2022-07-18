ff4c0974323b38712c1682b30ba5ce07c3828977
Divyanshu Parihar <divyanshu1447@gmail.com>
2022-07-18 20:40:06 +0530

    added initial styling setup with tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+        plugins: [
+            require('tailwindcss'),
+            require('autoprefixer'),
+        ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      primaryPink: "#ea526f",
+      smokyBlack: "#070600",
+      ghostWhite: "#f7f7ff",
+      pacificBlue: "#23b5d3",
+      dodgerBlue: "#279af1",
+    },
+    fontFamily:{
+      roboto : ['Roboto',sans-serif]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
